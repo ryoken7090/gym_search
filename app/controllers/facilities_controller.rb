2643de6d610e6b8615b66b0774a0bfc85f794085
ryoken nakayama <ryoken7090@gmail.com>
2020-08-11 00:50:26 +0900

    facilitiesコントローラー作成

---
+++ app/controllers/facilities_controller.rb
@@ -0,0 +1,13 @@
+class FacilitiesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'facilities/index'
+  get 'facilities/new'
+  get 'facilities/edit'
+  get 'facilities/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
