ef2d59b5fda9696a3531b8e4222350f31ccc3ca8
ryoken nakayama <ryoken7090@gmail.com>
2020-08-11 00:54:23 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'facilities/index'
-  get 'facilities/new'
-  get 'facilities/edit'
-  get 'facilities/show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :facilities
 end
