a2639a8efc56fe9c28d64f5a78381da6a4876eb8
ryoken nakayama <ryoken7090@gmail.com>
2020-08-11 16:18:29 +0900

    create機能作成

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -3,11 +3,38 @@ class FacilitiesController < ApplicationController
   end
 
   def new
+    @facility = Facility.new
+  end
+
+  def create
+    Facility.create(facility_params)
+    redirect_to new_facility_path
   end
 
   def edit
   end
 
   def show
+  end
+  def facility_params
+    params.require(:facility).permit(
+                                    :name,
+                                    :drop,
+                                    :parking,
+                                    :shower,
+                                    :recordable,
+                                    :visiter,
+                                    :monthly_fee,
+                                    :accessible_ten_min,
+                                    :trainer,
+                                    :open_all_time,
+                                    :station,
+                                    :description_station,
+                                    :address,
+                                    :tell,
+                                    :hp_url,
+                                    :regular_holiday,
+                                    :business_hours,
+                                    :description)
   end
 end
