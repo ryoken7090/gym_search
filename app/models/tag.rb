04488c76607538133bbb3f82911752da84585147
ryoken nakayama <ryoken7090@gmail.com>
2020-08-16 01:11:56 +0900

    タグ付機能作成

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
   validates :name, presence: true
   has_many :taggings, dependent: :destroy
-  has_many :tagging_facilities, through: :taggings, source: :facility
+  has_many :facilities, through: :taggings, source: :facility
 end
