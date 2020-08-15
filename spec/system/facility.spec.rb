6245da0489ee170718db84abcd206fb6da1e9e2d
ryoken nakayama <ryoken7090@gmail.com>
2020-08-15 15:36:14 +0900

    cityモデル追加のためfacilityシステムテスト修正

--- spec/system/facility.spec.rb
+++ spec/system/facility.spec.rb
@@ -1,22 +1,26 @@
 require 'rails_helper'
 RSpec.describe Facility, type: :system do
   before do
-    @city = FactoryBot.create(:city, name: 'テスト区')
+    @facility = FactoryBot.create(:city, name: 'テスト区')
+    FactoryBot.create(:city, name: 'テスト市')
   end
   describe 'Function to create new facility' do
     context 'when you input the name and and press the register button and press the register button on confirmation page' do
       it 'can save data' do
         visit new_facility_path
         fill_in '施設名', with: 'テスト施設'
+        select 'テスト区', from: 'facility_city_id'
         click_button '登録する'
         click_button '登録する'
         expect(page).to have_content 'テスト施設'
+        expect(page).to have_content 'テスト区'
       end
     end
     context 'when you input the name and and press the register button and press the back button on confirmation page' do
       it 'can not save data' do
         visit new_facility_path
         fill_in '施設名', with: 'テスト施設'
+        select 'テスト区', from: 'facility_city_id'
         click_button '登録する'
         click_button '戻る'
         click_link '施設一覧へ'
@@ -27,6 +31,7 @@ RSpec.describe Facility, type: :system do
       it 'register all items correctry' do
         visit new_facility_path
         fill_in '施設名', with: 'テスト施設'
+        select 'テスト区', from: 'facility_city_id'
         select '○', from: 'facility_drop'
         select '○', from: 'facility_parking'
         select '○', from: 'facility_shower'
@@ -44,6 +49,7 @@ RSpec.describe Facility, type: :system do
         fill_in '備考', with: '特になし'
         click_button '登録する'
         click_button '登録する'
+        expect(all('p')[0]).to have_content 'テスト区'
         expect(all('tbody tr')[0]).to have_content '○'
         expect(all('tbody tr')[1]).to have_content '○'
         expect(all('tbody tr')[2]).to have_content '○'
@@ -53,7 +59,7 @@ RSpec.describe Facility, type: :system do
         expect(all('tbody tr')[6]).to have_content '○'
         expect(all('tbody tr')[7]).to have_content '○'
         expect(all('tbody tr')[8]).to have_content '10000円'
-        expect(all('p')[0]).to have_content '特になし'
+        expect(all('p')[1]).to have_content '特になし'
         expect(all('tbody tr')[9]).to have_content '施設名：テスト施設'
         expect(all('tbody tr')[10]).to have_content '電話番号： 000-0000-0000'
         expect(all('tbody tr')[11]).to have_content '最寄り駅： 新宿駅'
@@ -66,9 +72,10 @@ RSpec.describe Facility, type: :system do
   describe 'Functuon to edit facirities' do
     context 'when edit some items' do
       it "update information" do
-        @facility = FactoryBot.create(:facility)
+        @facility = FactoryBot.create(:facility, city_id: @facility.id)
         visit facility_path(@facility)
         click_link '施設情報を編集する'
+        select 'テスト市', from: 'facility_city_id'
         select '✕', from: 'facility_drop'
         select '✕', from: 'facility_parking'
         select '✕', from: 'facility_shower'
@@ -86,6 +93,7 @@ RSpec.describe Facility, type: :system do
         fill_in '営業時間', with: '24時間'
         fill_in '備考', with: '特になし'
         click_button '更新する'
+        expect(all('p')[0]).to have_content 'テスト市'
         expect(all('tbody tr')[0]).to have_content '✕'
         expect(all('tbody tr')[1]).to have_content '✕'
         expect(all('tbody tr')[2]).to have_content '✕'
@@ -95,7 +103,7 @@ RSpec.describe Facility, type: :system do
         expect(all('tbody tr')[6]).to have_content '✕'
         expect(all('tbody tr')[7]).to have_content '✕'
         expect(all('tbody tr')[8]).to have_content '9000円'
-        expect(all('p')[0]).to have_content '特になし'
+        expect(all('p')[1]).to have_content '特になし'
         expect(all('tbody tr')[9]).to have_content '施設名：TEST_NAME1'
         expect(all('tbody tr')[10]).to have_content '電話番号： 000-0000-1234'
         expect(all('tbody tr')[11]).to have_content '最寄り駅： 渋谷駅'
