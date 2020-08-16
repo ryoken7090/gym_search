6620fe19ecccc65d5c23efa45ec6b71ef0e2b906
ryoken nakayama <ryoken7090@gmail.com>
2020-08-16 22:59:35 +0900

    ユーザーshowページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+        registrations: 'users/registrations'
+  }
+  resources :users, only: [:show]
   resources :facilities do
     collection do
       post :confirm
