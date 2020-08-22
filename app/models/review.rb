c745d6ef2a1e9a8c0419a0fad98722f7bb580268
ryoken nakayama <ryoken7090@gmail.com>
2020-08-23 00:07:16 +0900

    reviewの数値でバリデーション設定

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :facility
-  validates :free_weight, presence: true
-  validates :machine, presence: true
-  validates :caedio, presence: true
+  validates :free_weight, numericality: { greater_than: 0, less_than_or_equal_to: 5}, presence: true
+  validates :machine, numericality: { greater_than: 0, less_than_or_equal_to: 5}, presence: true
+  validates :caedio, numericality: { greater_than: 0, less_than_or_equal_to: 5}, presence: true
 end
