14956e8782b1dc7d3f0d63d0fa1e159338795f90
ryoken nakayama <ryoken7090@gmail.com>
2020-09-04 20:32:35 +0900

    簡単ログイン機能作成

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -84,6 +84,14 @@ class FacilitiesController < ApplicationController
     end
   end
 
+  def new_guest
+    user = User.find_or_create_by!(email: 'guest@example.com', name: 'ゲストユーザー') do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+    sign_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
+
   private
   def set_facility
     @facility = Facility.find(params[:id])
